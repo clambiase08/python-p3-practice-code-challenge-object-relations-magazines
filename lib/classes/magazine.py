de243bf97a08cca0bd88975271f29f8a7dad9076
Christina Lambiase <christina.lambiase@gmail.com>
2023-08-01 20:22:41 -0700

    init Magazine and fixed debug

--- lib/classes/magazine.py
+++ lib/classes/magazine.py
@@ -2,4 +2,22 @@ from classes.subscription import Subscription
 
 
 class Magazine:
-    pass
+    all = []
+
+    def __init__(self, title):
+        self.title = title
+        Magazine.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if isinstance(value, str):
+            self._title = value
+        else:
+            raise Exception("Title must be a string")
+
+    def __repr__(self):
+        return f"<Magazine: {self.title}>"
---
+++ lib/debug.py
@@ -0,0 +1,11 @@
+import ipdb
+
+from classes.magazine import Magazine
+from classes.reader import Reader
+from classes.subscription import Subscription
+
+
+vogue = Magazine("vogue")
+time = Magazine("time")
+
+ipdb.set_trace()
